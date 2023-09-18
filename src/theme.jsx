2d8d27dbe937155e9dabe75c3f47909a5100efcd
Ty <Tyu@example.com>
2023-09-18 21:57:34 +0700

    frame base

---
+++ src/theme.jsx
@@ -0,0 +1,24 @@
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles"
+import { teal, deepOrange, cyan, orange } from "@mui/material/colors"
+
+const theme = extendTheme({
+  trelloCustom: {
+    appBarHeight: "48px",
+    boardBarHeight: "58px",
+  },
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      },
+    },
+  },
+  dark: {
+    palette: {
+      primary: cyan,
+      secondary: orange,
+    },
+  },
+})
+export default theme
