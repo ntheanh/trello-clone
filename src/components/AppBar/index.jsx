cba08e57894abc1c80e1730eee5fbba6a1598fc9
Ty <Tyu@example.com>
2023-09-19 09:23:26 +0700

    refactor project structure

---
+++ src/components/AppBar/index.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material"
+import ModeSelect from "../../components/ModeSelect"
+const AppBar = () => {
+  return (
+    <Box
+      sx={{
+        backgroundColor: "primary.light",
+        width: "100%",
+        height: (theme) => theme.trelloCustom.appBarHeight,
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <ModeSelect />
+    </Box>
+  )
+}
+
+export default AppBar
